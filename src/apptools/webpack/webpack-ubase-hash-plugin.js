88a241d3b7774670ac11b6f8248ec0719dafd800
litor <547794529@qq.com>
2016-10-25 18:45:28 +0800

    新增webpack插件webpack-ubase-hash-plugin，用于在build中的静态资引用中加入hash信息

---
+++ src/apptools/webpack/webpack-ubase-hash-plugin.js
@@ -0,0 +1,42 @@
+'use strict';
+var ConcatSource = require("webpack-sources").ConcatSource;
+var fs = require('fs');
+function webpackUbaseHashPlugin() {
+
+}
+
+webpackUbaseHashPlugin.prototype.apply = function (compiler) {
+  compiler.plugin("emit", function (compilation, callback) {
+    var assets = Object.keys(compilation.assets);
+    Object.keys(compilation.assets).forEach(function (item) {
+      if (item.indexOf('index.html') == -1) {
+        return;
+      }
+
+      var appName = item.replace(/^([^\/]*)\/.*/, '$1');
+      var originHtml = compilation.assets[item]._value + '';
+      var hash = getHash(item, assets);
+      var newHtml = originHtml.replace(appName + '.' + 'js', appName + '-' + hash + '.js');
+
+      compilation.assets[item] = new ConcatSource(newHtml)
+    });
+
+    callback();
+  });
+};
+
+function getHash(indexhtmlFilePath, asset) {
+  var str = indexhtmlFilePath.replace(/\/index\.html/, '') + '\\\/([^\/]*)\.js$';
+  var re = new RegExp(str, "gi");
+  var hash = null;
+
+  asset.forEach(function (item) {
+    if (re.test(item)) {
+      hash = item.replace(/.*\-([^\/]*)\.js$/, '$1');
+      return false;
+    }
+  });
+  return hash;
+}
+
+module.exports = webpackUbaseHashPlugin;

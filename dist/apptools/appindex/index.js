9327f1e5d3a9a185de3700c1ebf92e401ccda5b5
litor <547794529@qq.com>
2016-11-15 16:07:58 +0800

    build

--- dist/apptools/appindex/index.js
+++ dist/apptools/appindex/index.js
@@ -1,27 +1,30 @@
 'use strict';
 
 // 初始化（获取config.json信息）
-window._UBASE_PRIVATE.init();
-// 初始化国际化信息
-'{{i18nimportTpl}}';
-'{{i18nsetValueTpl}}';
+require('../../ubase-vue');
+$(function () {
+  window._UBASE_PRIVATE.init();
+  // 初始化国际化信息
+  '{{i18nimportTpl}}';
+  '{{i18nsetValueTpl}}';
 
-require('{{indexHtml}}');
-require('{{config}}');
-var routes = require('{{routes}}').default;
+  require('{{indexHtml}}');
+  require('{{config}}');
+  var routes = require('{{routes}}').default;
 
-// 全局注册src/components及app下的vue组件
-'{{importTpl}}';
-'{{vueCompnentimportTpl}}';
+  // 全局注册src/components及app下的vue组件
+  '{{importTpl}}';
+  '{{vueCompnentimportTpl}}';
 
-var STORE = {
-  state: {},
-  actions: [],
-  mutations: [],
-  modules: {}
-};
+  var STORE = {
+    state: {},
+    actions: [],
+    mutations: [],
+    modules: {}
+  };
 
-'{{setValueTpl}}';
-'{{vueCompnentsetValueTpl}}';
+  '{{setValueTpl}}';
+  '{{vueCompnentsetValueTpl}}';
 
-window._UBASE_PRIVATE.startApp(null, STORE, routes);
+  window._UBASE_PRIVATE.startApp(null, STORE, routes);
+});

e8cdf0ab291cb7416e3cee18a0645335e0da232e
litor <547794529@qq.com>
2016-12-20 17:48:46 +0800

    优化日志显示

--- src/log.js
+++ src/log.js
@@ -17,9 +17,9 @@ function debugLog(string) {
 // Vue AJAX log 需要执行完Vue.use(VueResource)后才能初始化
 function initVueAjaxLog() {
   Vue.http.interceptors.push(function (request, next) {
-    debugLog(`[begin ajax] url: ${request.url}  request: ${JSON.stringify(request.body)}`)
+    debugLog(`[begin ajax] url: ${request.url}  request:\n ${JSON.stringify(request.body, null, 2)}`)
     next(function (response) {
-      debugLog(`[end ajax] url: ${response.url}  request: ${request.body} ` + (response.status !== 200 ? `http status: ${response.status}` : `response: ${JSON.stringify(response.body)} `))
+      debugLog(`[end ajax] url: ${response.url}  request: ${request.body} ` + (response.status !== 200 ? `http status: ${response.status}` : `response:\n ${JSON.stringify(response.body, null, 2)} `))
     });
   })
 }
@@ -38,10 +38,10 @@ Vue.mixin({
       var statesStringArray = []
 
       _.each(states, (item) => {
-        statesStringArray.push(`${item}: ${JSON.stringify(computed[item].bind(this)())}`)
+        statesStringArray.push(`${item}: ${JSON.stringify(computed[item].bind(this)(), null, 2)}`)
       })
 
-      debugLog(`[Vue Component Create] name: ${currentComponentName} state: \n-------------------------------------------------\n${statesStringArray.join('\n')}\n-------------------------------------------------`)
+      debugLog(`[Vue Component Create] name: ${currentComponentName} state: \n-------------------------------------------------\n${statesStringArray.join('\n\n')}\n-------------------------------------------------`)
     }
   },
 

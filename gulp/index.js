020fe9cdbc329f697b0cafebe5de0e8a92b7e534
litor <547794529@qq.com>
2017-02-24 14:07:26 +0800

    代码优化

--- gulp/index.js
+++ gulp/index.js
@@ -1,4 +1,5 @@
 import {setItem, getItem} from './localStorage'
+
 export default function (userConfig) {
   setItem('userConfig', JSON.stringify(userConfig))
   require('require-dir')('./tasks', { recurse: true })

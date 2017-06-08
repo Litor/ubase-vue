39519b21869e864d10fccb82e35bde21ea8db0b9
litor <547794529@qq.com>
2017-06-08 09:44:10 +0800

    clean时保留app_info文件夹

--- gulp/tasks/clean.js
+++ gulp/tasks/clean.js
@@ -5,7 +5,7 @@ import config from '../config'
 gulp.task('clean', cb => {
   var dest = config.dest
   try {
-    del.sync([dest + '/**/*', '!' + dest + '/WEB-INF/**'], {force: true})
+    del.sync([dest + '/**/*', '!' + dest + '/WEB-INF/**', '!' + dest + '/APP_INFO/**'], {force: true})
   } catch (e) {
     console.log('%s do not clean', dest)
   }
